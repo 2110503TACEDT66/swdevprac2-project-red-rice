869e7dbfe655850dd7aa58b509074878d2ad1c29
sakdipat suklhaneskul <jpn.shutter@gmail.com>
2024-03-21 13:32:49 +0700

    adminpage component

---
+++ src/app/admin/manage/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import CardPanel from "@/components/adminmanage/cardpanel"
+export default function AdminManage(){
+    
+    return (
+        <main className="relative top-[100px]">
+            <CardPanel></CardPanel>
+        </main>
+    )
+}
