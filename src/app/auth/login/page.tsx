a65c895ef1d83df3b4faef7e44bacfb3cc72114e
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-22 18:41:16 +0700

    feat: add register page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <>
+            <main className="flex items-center justify-center">
+                <section>
+                    <header>
+                        <h2>Red Rice</h2>
+                        <p>A restaurant booking platform.</p>
+                    </header>
+                    <section>
+                        <span>
+                            Reserve Your Table for <br></br> an Unforgettable{' '}
+                            <br></br> Dining Experience
+                        </span>
+                    </section>
+                </section>
+                <section>
+                    <h2>Create Account</h2>
+
+                    <span>
+                        Already Have an account? <span>Sign in</span>
+                    </span>
+                </section>
+            </main>
+        </>
+    );
+};
+
+export default RegisterPage;
