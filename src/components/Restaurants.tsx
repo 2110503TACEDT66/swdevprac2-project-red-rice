88348f3e71b6adc3f9886dde4c0206f592287544
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-25 03:08:20 +0700

    test: on get all restaurant

--- src/components/Restaurants.tsx
+++ src/components/Restaurants.tsx
@@ -1,13 +1,26 @@
+'use client';
 import React from 'react';
 import RestaurantCard from '@/components/RestaurantCard';
-import { getServerSession } from 'next-auth';
+import { useSession } from 'next-auth/react';
 import { getAllRestaurant } from '@/lib/restaurant';
-const Restaurants = async () => {
-    const session = await getServerSession();
-    const restaurants = await getAllRestaurant(session?.user.token ?? '');
+import { useEffect, useState } from 'react';
+const Restaurants = () => {
+    const { data: session } = useSession();
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        const fetchRestaurants = async () => {
+            if (session?.user.token) {
+                const fetchedRestaurants = await getAllRestaurant(session.user.token);
+                setRestaurants(fetchedRestaurants);
+            }
+        };
+        fetchRestaurants();
+    }, [session]);
+    
     return (
         <div className="mt-5 md:mt-14 h-2/3 max-h-[550px] overflow-x-scroll ring-2 ring-slate-200 rounded-xl p-5 flex flex-row">
-            {restaurants.map((res: any) => (
+            {restaurants.map((res: Restaurant) => (
                 <RestaurantCard key={res.id} name = {res.name} imageUrl={res.imageUrl} id = {res.id}/>
             ))}
         </div>
---
+++ src/types/restaurant.ts
@@ -0,0 +1,13 @@
+interface Restaurant {
+    id: string;
+    name: string;
+    address: string;
+    description: string;
+    telephone: string;
+    openTime: string;
+    closeTime: string;
+    imageUrl: string;
+    instagram: string;
+    facebook: string;
+    email: string;
+}
