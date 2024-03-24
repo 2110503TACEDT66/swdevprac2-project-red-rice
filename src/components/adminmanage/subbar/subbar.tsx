abac8f07eb566189682086e8d13dabf012b1a5b5
punchpnp <nunnapatppp@gmail.com>
2024-03-24 20:39:12 +0700

    fix: subbar

--- src/components/adminmanage/subbar/subbar.tsx
+++ src/components/adminmanage/subbar/subbar.tsx
@@ -2,9 +2,9 @@ import Searchbox from '@/components/Searchbox';
 
 export default function SubBar() {
     return (
-        <div className="flex flex-row justify-between items-center">
-            <div className='flex flex-row items-center w-full'>
-                <h1 className="text-3xl md:text-4xl font-semibold w-full">
+        <div className="flex flex-row justify-between items-center flex-wrap">
+            <div className='flex flex-row items-center'>
+                <h1 className="text-3xl md:text-4xl font-semibold">
                     User Management
                 </h1>
             </div>
