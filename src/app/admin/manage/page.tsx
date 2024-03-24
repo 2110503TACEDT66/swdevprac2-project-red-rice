ad4db58280595be877eb9524c3c64fb77d0704e7
punchpnp <nunnapatppp@gmail.com>
2024-03-24 21:39:41 +0700

    delete: footer

--- src/app/admin/manage/page.tsx
+++ src/app/admin/manage/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardPanel from '@/components/adminmanage/cardpanel';
 import SubBar from '@/components/adminmanage/subbar/subbar';
-import Footer from '@/components/adminmanage/footer/footer';
+
 export default function AdminManage() {
     return (
         <main className="pl-12 pr-10 w-full h-auto overflow-y-auto">
