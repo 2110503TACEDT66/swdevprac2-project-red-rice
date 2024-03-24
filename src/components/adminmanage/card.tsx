0b8dac2278856d9cbcce9b9643bd178eb599b719
punchpnp <nunnapatppp@gmail.com>
2024-03-24 21:06:48 +0700

    fix: card component

--- src/components/adminmanage/card.tsx
+++ src/components/adminmanage/card.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { IoInformation } from 'react-icons/io5';
 
 interface CardProps {
     id: string;
@@ -9,28 +10,47 @@ interface CardProps {
     tel: string;
     picture?: string;
 }
-export default function Card({ id, name, role, email, tel, picture }: CardProps) {
+export default function Card({
+    id,
+    name,
+    role,
+    email,
+    tel,
+    picture,
+}: CardProps) {
     return (
-        <div className="h-auto w-full rounded-[1rem] shadow-md m-1 flex flex-row border-2 py-3 items-center">
-            <div className="w-[55px] h-[55px] relative rounded-full  p-7 m-3 ml-5 mr-5 border-solid border-2 border-slate-400 self-center ">
-                <Image
-                    src={picture || '/img/user/user1.png'}
-                    alt="Product Picture"
-                    fill={true}
-                    className="object-cover rounded-full "
-                />
+        <div className="h-auto w-full rounded-[1rem] shadow-md m-2 flex flex-row py-2 md:py-7 items-center justify-between pr-5 pl-5 border-2">
+            <div className="flex flex-row items-center">
+                <h1 className='mr-5 md:mr-10'>{id}</h1>
+                <div className="flex flex-row gap-5 md:gap-10 mr-3 md:mr-10">
+                    <Image
+                        src={picture || '/img/user/user1.png'}
+                        alt="Product Picture"
+                        width={50}
+                        height={50}
+                        className="object-cover rounded-full"
+                    />
+                    <div className="flex flex-col">
+                        <h1>{name}</h1>
+                        <h1 className="text-slate-400">{role}</h1>
+                    </div>
+                </div>
+                <div className="hidden md:flex flex-row gap-10 lg:gap-20 items-center">
+                    <h1>{email}</h1>
+                    <h1>{tel}</h1>
+                </div>
             </div>
-            <div className="flex flex-col w-[100px] h-[10%] self-center">
-                <div className="">{name}</div>
-                <div className="text-slate-400">{role}</div>
-            </div>
-            <div className="w-[200px] h-[10%] self-center ">{email}</div>
-            <div className="w-[100px] h-[10%] self-center">{tel}</div>
-            <div className="justify-end">
+            <div className='md:hidden'>
+            <Link href={`../${id}/profile`}>
+                <button className='p-1 rounded-full bg-redrice-yellow hover:bg-redrice-light-yellow text-white'>
+                <IoInformation />
+                </button>
+                    
+                </Link>
+                </div>
+            <div className="hidden md:flex">
                 <Link href={`../${id}/profile`}>
-                    <button
-                        className="block h-[10%] w-[150px] text-sm rounded-full bg-redrice-yellow hover:bg-redrice-light-yellow  p-1 m-3  shadow-sm text-white text-center"
-                    >
+                    <button className="block text-md font-semibold rounded-full bg-redrice-yellow hover:bg-redrice-light-yellow  px-6 py-3 shadow-sm text-white text-center">
                         view details
                     </button>
                 </Link>
