96e8f413eb439c7f987591d4f4d148c9ce835b1f
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-26 02:01:04 +0700

    fix reservation image

--- src/components/reservation/reservationpanel.tsx
+++ src/components/reservation/reservationpanel.tsx
@@ -23,7 +23,7 @@ export default function ReservationPanel({ data }: { data: reservation[] }) {
                     time={item.dateTime.split('T')[1].substring(0, 5)}
                     state={'Pending'}
                     picture={
-                        'https://i.pinimg.com/originals/5c/a7/3a/5ca73a010de31c834165317c1feb22d3.jpg'
+                        item.restaurant.imageUrl
                     }
                 ></ReservationCard>
             ))}
