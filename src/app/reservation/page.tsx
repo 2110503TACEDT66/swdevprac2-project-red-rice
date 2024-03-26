2021903945bc795fc4dcde087b2c739fe01ca882
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-26 22:55:19 +0700

    fix: get reservation

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -2,22 +2,34 @@
 import SubBar from '@/components/subbar';
 import ReservationPanel from '@/components/reservation/reservationpanel';
 import { reservation } from '../../../interface';
-import { getMyReservations } from '@/lib/reservation';
+import { getme } from '@/lib/auth';
+import { getMyReservations, getAllResvation } from '@/lib/reservation';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { CircularProgress } from '@mui/material';
 export default function Reservation() {
     const { data: session } = useSession();
     const [reservationData, setReservations] = useState<reservation[]>();
+    const [role, setRole] = useState<string>('');
     useEffect(() => {
         if (!session) return;
-        const fetchReservations = async () => {
-            const fetchedReservations = await getMyReservations(
-                session.user.token
-            );
-            setReservations(fetchedReservations);
+        const getMe = async () => {
+            const me = await getme(session.user.token);
+            setRole(me.role);
+            return me.role; // Return role for immediate use
+        }
+    
+        const fetchReservations = async (role: string) => {
+            if (role === 'user') {
+                const fetchedReservations = await getMyReservations(session.user.token);
+                setReservations(fetchedReservations);
+            } else if (role === 'admin') {
+                const fetchedReservations = await getAllResvation(session.user.token);
+                setReservations(fetchedReservations);
+            }
         };
-        fetchReservations();
+    
+        getMe().then(fetchReservations); // Chain the promise
     }, [session]);
 
     return (
