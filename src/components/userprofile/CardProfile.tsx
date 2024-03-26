cd4913f047f49e03c0ce9c971734df9dd730c439
punchpnp <nunnapatppp@gmail.com>
2024-03-26 12:57:14 +0700

    fix: profile icon

--- src/components/userprofile/CardProfile.tsx
+++ src/components/userprofile/CardProfile.tsx
@@ -1,24 +1,18 @@
-'use client'
+'use client';
 import Image from 'next/image';
-export default function CardProfile({profile}:{profile:any}) 
-
-{
-
-    const picture = profile?.picture
+import { FaUserAlt } from 'react-icons/fa';
+export default function CardProfile({ profile }: { profile: any }) {
+    const picture = profile?.picture;
     return (
-        <div className=" w-[95%] rounded-lg shadow-md m-3 flex tablet:flex-row flex-col  ">
-            <div className="w-[55px] h-[55px] relative rounded-full  p-10 m-3 ml-7  self-center ">
-                <Image
-                    src={picture || '/img/user/user1.png'}
-                    alt="Product Picture"
-                    fill={true}
-                    className="object-cover rounded-full "
-                />
+        <div className="w-full rounded-lg shadow-md flex flex-row border-2 mt-5">
+            <div className="m-5 relative rounded-full border-4 border-black hover:border-redrice-yellow p-2 text-2xl hover:text-redrice-yellow">
+                <FaUserAlt />
             </div>
+
             <div className="w-[60%]  relative right-0  m-3 self-center flex flex-col tablet:items-start items-center">
-                    <h1 >{profile.name}</h1>
-                    {profile.edu?<h1>profile.edu</h1>:''}
-                    {profile.bdy?<h1>profile.bdy</h1>:''}
+                <h1>{profile.name}</h1>
+                {profile.edu ? <h1>profile.edu</h1> : ''}
+                {profile.bdy ? <h1>profile.bdy</h1> : ''}
             </div>
         </div>
     );
--- src/components/userprofile/reservation.tsx
+++ src/components/userprofile/reservation.tsx
@@ -3,7 +3,7 @@ import CardReservationPanel from './CardReservationPanel';
 export default async function Reservation({data}:{data:any}) {
     
     return (
-        <div className="tablet:w-[65%]  m-3 rounded-lg shadow-lg flex flex-col w-[95%] ">
+        <div className="w-full md:w-2/3 h-[500px] rounded-lg shadow-lg flex flex-col border-2 overflow-auto mt-2">
             <h2 className="m-5 mb-3 relative top-[0px] left-[0%]">
                 Reservation
             </h2>
